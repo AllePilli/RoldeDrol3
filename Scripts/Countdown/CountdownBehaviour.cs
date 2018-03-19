e46fc3c4d250084503b9b9d5e44378beb6fad103
Allesio Pellicciotta <allesio.pellicciotta@gmail.com>
2018-03-19 13:53:02 +0100

    Added start flag and start animations

---
+++ Scripts/Countdown/CountdownBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountdownBehaviour : MonoBehaviour {
+
+	private Animator animator;
+	private Transform transform;
+
+	void Start () {
+		animator = GetComponent<Animator>();
+		transform = GetComponent<Transform>();
+	}
+
+	void Update () {
+
+	}
+
+	public void StartAnimation(){
+		animator.SetBool("startCountdown", true);
+		transform.position = new Vector3(6.2f, 0f, 0f);
+	}
+
+	void EndAnimation(){
+		animator.SetBool("startCountdown", false);
+		transform.position = new Vector3(6.2f, 26.8f, 0f);
+	}
+}
---
+++ Scripts/Start/StartBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartBehaviour : MonoBehaviour {
+
+	public GameObject countdown;
+
+	void Start () {
+
+	}
+
+	void Update () {
+
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		countdown.GetComponent<CountdownBehaviour>().StartAnimation();
+	}
+}
