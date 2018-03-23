82154ed43ed81553e3244cb89d89ac940a07ece2
Allesio Pellicciotta <allesio.pellicciotta@gmail.com>
2018-03-23 10:58:13 +0100

    Added Timer class

---
+++ Scripts/Timer/Timer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Timer{
+	//Bron: https://coffeebraingames.wordpress.com/2014/10/20/a-generic-duration-timer-class/
+	private float polledTime;
+	private float duration;
+
+	public Timer(float duration){
+		Reset(duration);
+	}
+
+	public void Update(){
+		polledTime += Time.deltaTime;
+	}
+
+	public void Reset(){
+		polledTime = 0;
+	}
+
+	public void Reset(float duration){
+		Reset();
+		this.duration = duration;
+	}
+
+	public bool Done(){
+		return Comparison.TolerantGreaterThanOrEquals(polledTime, duration);
+	}
+
+	public float GetPolledTime(){
+		return polledTime;
+	}
+
+	public float GetDuration(){
+		return duration;
+	}
+}
