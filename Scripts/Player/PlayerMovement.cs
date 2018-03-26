469c5cf44ac85b9e0d981b843709f162f7cc6c77
Allesio Pellicciotta <allesio.pellicciotta@gmail.com>
2018-03-26 12:09:41 +0200

    Debug

--- Scripts/Player/PlayerMovement.cs
+++ Scripts/Player/PlayerMovement.cs
@@ -1,25 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO.Ports;
-using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
 	private Rigidbody2D playerBody;
 	private Animator animator;
-	private float arduinoInput;
+	public string arduinoInput, prevInput;
 	private Vector2 initialPosition;
 	private Timer timer;
 	private AudioManager audioManager;
+	//private SerialPort sp;
 
 	[SerializeField]
 	private float maxSpeed, resetTime;
 
 	public bool finished, frozen, started;
+	public float input;
+	public bool DEMO = false;
+	public bool serialClosed;
 
 	//Met Arduino
+	private SerialPort sp = new SerialPort("COM7", 9600);
 	//private SerialPort sp = new SerialPort("COM5", 9600);
+
 
 	void Start () {
 		playerBody = GetComponent<Rigidbody2D>();
@@ -30,22 +36,40 @@ public class PlayerMovement : MonoBehaviour {
 		initialPosition = playerBody.position;
 
 		/*Met Arduino
+		if(!DEMO){
+			sp = new SerialPort("COM4", 9600);
+			sp.Open();
+			sp.ReadTimeout = 1;
+		}*/
+
 		sp.Open();
-		sp.ReadTimeout = 1;*/
+		sp.ReadTimeout = 55;// 1
 	}
 
 	// Update is called once per frame
 	void Update () {
 		/*Met Arduino
+		if(!DEMO){
+			if(sp.IsOpen){
+				try {
+					//arduinoInput = sp.ReadByte();
+					arduinoInput = sp.ReadLine();
+				}catch (System.Exception) {
+					throw;
+				}
+			}
+		}*/
+
 		if(sp.IsOpen){
 			try {
-				arduinoInput = sp.ReadByte();
+				//arduinoInput = sp.ReadByte();
+				arduinoInput = sp.ReadLine();
 			}catch (System.Exception) {
-				throw;
+				arduinoInput = prevInput;
 			}
-
-			playerBody.velocity = new Vector2(arduinoInput * maxSpeed, playerBody.velocity.y);
-		}*/
+		}else{
+			serialClosed = true;
+		}
 
 		if(started && (Comparison.TolerantEquals(playerBody.velocity.x, 0) || finished)){
 			timer.Update();
@@ -57,9 +81,14 @@ public class PlayerMovement : MonoBehaviour {
 			timer.Reset();
 		}
 
-		float input = Input.GetAxis("Horizontal");
-
-		HandleMovement(input);
+		/*if(DEMO){
+			input = Input.GetAxis("Horizontal");
+			HandleMovement(input);
+		}else{
+			HandleMovement(float.Parse(arduinoInput));
+		}*/
+		prevInput = arduinoInput;
+		HandleMovement(float.Parse(arduinoInput));
 	}
 
 	private void HandleMovement(float input){
