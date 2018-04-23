3c374f1a59c168a34ad35e8dd6975f55ba12916b
Allesio Pellicciotta <allesio.pellicciotta@gmail.com>
2018-04-23 15:13:52 +0200

    Screen resolution is read from file

---
+++ Scripts/Player/ScreenResolution.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class ScreenResolution : MonoBehaviour {
+
+	void Awake () {
+        string line = readResolution();
+        string[] parts = line.Split(',');
+
+        Screen.SetResolution(int.Parse(parts[0]), int.Parse(parts[1]), true);
+	}
+
+    private string readResolution()
+    {
+        try
+        {
+            StreamReader reader = new StreamReader(Application.dataPath + "/Resolution.txt");
+            string line = reader.ReadToEnd();
+            reader.Close();
+            return line;
+        }
+        catch
+        {
+            string line = "1280,768";
+            return line;
+        }
+    }
+}
