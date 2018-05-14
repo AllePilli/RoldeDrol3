3a9098fb59ef820ceeb966f9033be2e2c13b4251
Allesio Pellicciotta <allesio.pellicciotta@gmail.com>
2018-05-14 11:38:06 +0200

    Changed enemy camera movement

---
+++ Scripts/Enemy/CameraEnemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraEnemy : MonoBehaviour {
+	public Transform player;
+	public Vector3 offset_a;
+	public float correction;
+
+	private Vector3 initialPosition;
+
+	/*
+		initialises data members
+	*/
+	void Start () {
+		initialPosition = GetComponent<Transform>().position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = new Vector3(player.position.x + correction, initialPosition.y, 0);
+		transform.position = pos + offset_a;
+	}
+}
